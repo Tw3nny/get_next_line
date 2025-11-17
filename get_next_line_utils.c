46b480b71479ff544356e03a9e691c77502bc986
Matis Gutierrez <matisgutierreztw3nny@MacBook-Air-de-Matis.local>
2025-11-17 13:00:35 +0100

    done

---
+++ get_next_line_utils.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matisgutierreztw3nny <matisgutierreztw3    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 10:59:39 by matisgutier       #+#    #+#             */
+/*   Updated: 2025/11/17 11:24:39 by matisgutier      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char) c)
+			return ((char *) &str[i]);
+		i++;
+	}
+	if (str[i] == (char) c)
+		return ((char *) &str[i]);
+	return (0);
+}
+
+char	*ft_substr(const char *str, unsigned int start, size_t len)
+{
+	int		i;
+	int		str_len;
+	char	*substr;
+
+	str_len = ft_strlen(str);
+	if (!str)
+		return (NULL);
+	if (start > str_len)
+		return (malloc(1));
+	if (start + str_len > len)
+		len = str_len - start;
+	substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = str[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	*strdup(char *str)
+{
+	int		i;
+	int		size;
+	char	*dup;
+
+	size = ft_strlen(str);
+	dup = malloc(size + 1);
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		dup[i] = str[i],
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+
