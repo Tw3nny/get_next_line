a819478597b370148248f526eff0f1f17c8cf0bc
Tw3nny <boxdedarkfighter@gmail.com>
2025-12-02 23:52:17 +0100

    done

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matisgutierreztw3nny <matisgutierreztw3    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 13:58:35 by matisgutier       #+#    #+#             */
-/*   Updated: 2025/11/26 11:48:22 by matisgutier      ###   ########.fr       */
+/*   Updated: 2025/12/02 23:51:24 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@
 // cherche le \n
 char	*ft_strchr(const char *str, int c)
 {
-	int i;
+	int	i;
+
 	i = 0;
 	while (str[i] != '\0')
 	{
@@ -39,11 +40,13 @@ char	*ft_strchr(const char *str, int c)
 		return ((char *) &str[i]);
 	return (0);
 }
-// lit la ligne jusqu'a tomber sur \n + return stash complet
-char	*fill_line_buffer(int fd, char *stash, char *buffer)
+// lit la ligne jusqu'a tomber sur \n
+
+static char	*read_line(int fd, char *stash, char *buffer)
 {
-	ssize_t bytes_read;
+	ssize_t	bytes_read;
 	char	*tmp;
+
 	if (!stash)
 		stash = ft_strdup("");
 	if (!stash)
@@ -67,10 +70,11 @@ char	*fill_line_buffer(int fd, char *stash, char *buffer)
 	return (stash);
 }
 // si on trouve pas de newline dans set_line
-char	*no_new_line(char **stash_ptr)
+
+static char	*no_new_line(char **stash_ptr)
 {
 	char	*line;
-	
+
 	if (!*stash_ptr || **stash_ptr == '\0')
 	{
 		if (*stash_ptr)
@@ -83,13 +87,16 @@ char	*no_new_line(char **stash_ptr)
 	*stash_ptr = NULL;
 	return (line);
 }
-static char *set_line(char **stash_ptr)
+
+// decoupe/ecrit la line
+static char	*set_line(char **stash_ptr)
 {
 	char	*stash;
 	char	*newline;
 	char	*line;
 	char	*tmp;
 	int		len;
+
 	stash = *stash_ptr;
 	if (!stash || *stash == '\0')
 		return (no_new_line(stash_ptr));
@@ -108,17 +115,19 @@ static char *set_line(char **stash_ptr)
 	}
 	return (line);
 }
+
 char	*get_next_line(int fd)
 {
 	char		*line;
 	char		*buffer;
-	static char *stash;
+	static char	*stash;
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-	stash = fill_line_buffer(fd, stash, buffer);
+	stash = read_line(fd, stash, buffer);
 	free(buffer);
 	if (!stash)
 		return (NULL);
@@ -126,39 +135,37 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-int main(void)
+/* int main(void)
 {
 	int fd;
 	char *line;
-	
-	system("printf '\\n\\n\\n\\n\\n' > test.txt");
-	fd = open("test.txt", O_RDONLY);
-	
+
+	fd = open("test1.txt", O_RDONLY);
+
 	line = get_next_line(fd);
 	printf("1: %s", line ? line : "NULL\n");
 	free(line);
-	
+
 	line = get_next_line(fd);
 	printf("2: %s", line ? line : "NULL\n");
 	free(line);
-	
+
 	line = get_next_line(fd);
 	printf("3: %s", line ? line : "NULL\n");
 	free(line);
-	
+
 	line = get_next_line(fd);
 	printf("4: %s", line ? line : "NULL\n");
 	free(line);
-	
+
 	line = get_next_line(fd);
 	printf("5: %s", line ? line : "NULL\n");
 	free(line);
-	
+
 	line = get_next_line(fd);
 	printf("6: %s", line ? line : "NULL\n");
 	free(line);
-	
+
 	close(fd);
-	unlink("test.txt");
 	return (0);
-}
+} */
